3cb93bd70ce05b22500103727cb49428d5a003eb
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2023-08-12 20:29:40 +0900

    fixed some problems

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt update &&
 sudo apt install python3-colcon-common-extensions ros-humble-ros2-control \
-ros-humble-ros2-controllers ros-humble-gazebo-* &&
+ros-humble-ros2-controllers &&
 cd gtsam && mkdir build && cd build && cmake .. && make && sudo make install
 ##the below one is for ubuntu 20.04 
 #sudo add-apt-repository ppa:borglab/gtsam-release-4.0 &&
